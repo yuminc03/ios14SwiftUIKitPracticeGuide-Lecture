b751e6db6b9b661760297ce7686ac9c7b2e0896b
Yumini <60338309+yuminc03@users.noreply.github.com>
2022-01-20 23:18:51 +0900

    2022-1-20

---
+++ SettingCloneApp/SettingCloneApp/SettingTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  SettingTableViewCell.swift
+//  SettingCloneApp
+//
+//  Created by Chu Yu Min on 2022/01/18.
+//
+
+import UIKit
+
+class SettingTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var topTitleLabel: UILabel!
+    @IBOutlet weak var bottomTitleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        profileImageView.layer.cornerRadius = profileImageView.frame.width / 2
+        topTitleLabel.textColor = .systemBlue
+        topTitleLabel.font = .systemFont(ofSize: 20)
+        bottomTitleLabel.textColor = .systemGray
+        bottomTitleLabel.font = .systemFont(ofSize: 16)
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+    }
+    
+}
